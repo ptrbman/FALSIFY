f51381d24a04e2d45832a9e44060e5dc468de0d4
Peter Backeman <peter@backeman.se>
2022-01-28 12:28:29 +0100

    Add stack example

---
+++ examples/stack.c
@@ -0,0 +1,23 @@
+#define STACK_SIZE 3
+int stack[STACK_SIZE];
+int stack_ptr = 0;
+
+int pop(int *elem) {
+  if (stack_ptr == 0) {
+    return 1;
+  } else {
+    stack_ptr -= 1;
+    *elem = stack[stack_ptr];
+    return 0;
+  }
+}
+
+int push(int elem) {
+  if (stack_ptr < STACK_SIZE) {
+    stack[stack_ptr] = elem;
+    stack_ptr += 1;
+    return 0;
+  } else {
+    return 1;
+  }
+}
